d4548364b1b312204da1930b4aa5fb3555bfd072
David Kwan <daves.kwan@gmail.com>
2019-01-05 14:58:34 -0500

    intitial commit

--- scripts/docker_push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
-docker push davidkwan/multi-container-deployment-client
-docker push davidkwan/multi-container-deployment-nginx
-docker push davidkwan/multi-container-deployment-server
-docker push davidkwan/multi-container-deployment-worker
